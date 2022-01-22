aaa07a395f6074ed04cd067fc11b68cdaea7dc54
Alex Hanina <lexiganina@gmail.com>
2022-01-23 01:01:23 +0200

    Ball logic done

--- MagicBall/Controllers/BallViewController.swift
+++ MagicBall/Controllers/BallViewController.swift
@@ -6,14 +6,78 @@
 //
 
 import UIKit
+import AudioToolbox
+import Reachability
 
-class ViewController: UIViewController {
-
+class BallViewController: UIViewController {
+    
+    @IBOutlet weak var answerLable: UILabel!
+    
+    private var answersDataResult: AnswersData?
+    private var internetStatus = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        NetworkMonitor.shared.delegate = self
     }
+    
+    //    MARK: - Data request
+    func updateModelWithData() {
+        if internetStatus {
+            NetworkManager.shared.downloadAnswersData { answersData in
+                DispatchQueue.main.async {
+                    self.answersDataResult = answersData
+                    self.answerLable.text = self.getAnswerFromInternet()
+                }
+                
+            } onError: { error in
+                print(error)
+                DispatchQueue.main.async {
+                    self.answerLable.text = self.getRandomeAnswer()
+                }
+            }
+        } else {
+            self.answerLable.text = self.getRandomeAnswer()
+        }
+    }
+    
+    //    MARK: - Gesture
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+            updateModelWithData()
+        }
+    }
+    
+    //    MARK: - Ball Logic
+    func getAnswerFromInternet() -> String {
+        if let answer = answersDataResult?.magic.answer {
+            print("internet answer")
+            return answer
+        } else {
+            print("Data decoding failed")
+            return getRandomeAnswer()
+        }
+    }
+    
+    func getRandomeAnswer() -> String {
+        let randomAnswer = RandomAnswers.answersArray[Int.random(in: 0 ..< RandomAnswers.answersArray.count)]
+        print("random answer")
+        return randomAnswer
+    }
+    
+}
 
-
+extension BallViewController: NetworlMonitorDelegate {
+    func didConnected() {
+        internetStatus = true
+    }
+    
+    func didDisconected() {
+        internetStatus = false
+    }
+    
+    
 }
 
---
+++ MagicBall/Model/RandomAnswersData.swift
@@ -0,0 +1,31 @@
+//
+//  RandomAnswersData.swift
+//  MagicBall
+//
+//  Created by Alex Hanina on 1/22/22.
+//
+
+import Foundation
+
+struct RandomAnswers {
+    
+    static var answersArray = ["It is certain", "It is decidedly so",
+                               "Without a doubt",
+                               "Yes — definitely",
+                               "You may rely on it",
+                               "As I see it, yes",
+                               "Most likely",
+                               "Outlook good",
+                               "Signs point to yes",
+                               "Yes",
+                               "Reply hazy, try again",
+                               "Ask again later",
+                               "Better not tell you now",
+                               "Cannot predict now",
+                               "Ask again",
+                               "Don’t count on it",
+                               "My reply is no",
+                               "My sources say no",
+                               "Outlook not so good",
+                               "Very doubtful"]
+}
